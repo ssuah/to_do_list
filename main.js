a31c6e8ed5b5a38a17f4ab9c10ac4939a79c5638
suah lee <suah216@gmail.com>
2023-03-14 01:21:39 +0900

    first

---
+++ main.js
@@ -0,0 +1,68 @@
+let taskInput =document.getElementById("task-input");
+let addButton=document.getElementById("add-button");
+let taskList=[];
+
+addButton.addEventListener("click",addTask);
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+function addTask(){
+
+    let task={
+        id:randomIDGenerate(),
+        taskContent:taskInput.value,
+        isComplete:false
+    }
+    taskList.push(task);
+    console.log(taskList);
+    render();
+}
+function render(){
+    let resultHTML="";
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].isComplete==true){
+            resultHTML+=`<div class="task">
+            <div class="task-done">${taskList[i].taskContent}</div>    
+               <div>
+                   <button onclick="toggleComplete('${taskList[i].id}')">check</button>
+                   <button onclick="deleteTask('${taskList[i].id}')">delete</button>
+               </div>
+           </div>`;
+        }else{
+            resultHTML+=`<div class="task">
+           <div>${taskList[i].taskContent}</div>    
+             <div>
+                 <button onclick="toggleComplete('${taskList[i].id}')">check</button>
+                 <button onclick="deleteTask('${taskList[i].id}')">delete</button>
+             </div>
+           </div>`;
+        }
+        
+    }
+
+    document.getElementById("task-board").innerHTML=resultHTML;
+    
+    
+}
+function toggleComplete(id){
+    console.log("id",id);
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
+}
+function deleteTask(id){
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList.splice(i,1);
+            break;
+        }
+    }
+    render();
+}
